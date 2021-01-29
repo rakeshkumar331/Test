42429d57ae26df23501f698e34f4af6696d72f2e
rakeshkumar331 <rakesh.kumar@kelltontech.com>
2021-01-29 23:03:41 +0530

    first commit

---
+++ src/components/CustomerApp.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="customer-app">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Welcome to Customer App',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
